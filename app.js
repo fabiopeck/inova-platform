251b7d5c8c5b3f818b035b83b2403f772255c3e8
fabiopeck <FABIOPECK86@GMAIL.COM>
2026-02-01 13:46:28 -0300

    app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+<script src="app.js"></script>
+<script>
+  window.addEventListener('load', function() {
+      requestNotificationPermission();
+      registerUserToken();
+      showCookieBanner();
+  });
+</script>
